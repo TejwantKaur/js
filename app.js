1c9c5b701086977ac13768e0f22d469a42935cf7
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-14 17:28:35 +0530

    handle promise

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ h1 = document.querySelector("h1");
 function changeColor(color, delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+            let num = Math.floor(Math.random()*5)+1;
+            if(num>3){
+                reject("Promise rejected");
+            }
             h1.style.color = color;
+            console.log(`color changed to ${color}`);
             resolve("Color changed");
         },delay);
     });
 }
 
 async function demo(){
-    await changeColor("red", 1000);
-    await changeColor("green",1000);
-    await changeColor("Orange",1000);
-    changeColor("blue",1000);
+    try{
+        await changeColor("red", 1000);
+        await changeColor("green",1000);
+        await changeColor("Orange",1000);
+        await changeColor("blue",1000);
+    } catch(err){
+        console.log("error caught", err);
+    }
+    let a = 5;
+    console.log("after promise print 5");
 }
 
 function getNum(){
