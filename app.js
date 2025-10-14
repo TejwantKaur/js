f9862a91e86e9e0f9ee862b72e65e0d2f1e6acbc
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-14 16:52:20 +0530

    Promises

--- app.js
+++ app.js
@@ -1,81 +1,29 @@
-async function greet(){
-    // throw "404 pg not found";
-    return "hello";
+function savetoDb(data){
+    return new Promise((resolve,reject)=>{
+        let speed = Math.floor(Math.random()*10)+1;
+        if(speed>4)
+            resolve("Success: Data Saved");
+        else
+            reject("Failure: Weak Connection, Data not Saved");
+    });
 }
 
-greet()
-    .then((result) =>{
-        console.log("Promise resolved");
-        console.log("Result was",result);
+savetoDb("Data1")
+    .then((result)=>{
+        console.log("data1Saved");
+        console.log("Result of Promise: ", result);
+        return savetoDb("Data2");
     })
-    .catch((err)=>{
-        console.log("Promise rejected with error",err);
+    .then((result)=>{
+        console.log("data2Saved");
+        console.log("Result of Promise: ", result);
+        return savetoDb("Data3");
+    })
+    .then((result)=>{
+        console.log("data3Saved");
+        console.log("Result of Promise: ", result);
+    })
+    .catch((error)=>{
+        console.log("Promise Rejected!");
     });
-
-let demo = async()=>{
-    return 5;
-};
-
-
-
-
-
-
-// h1 = document.querySelector("h1");
-
-// function colorChange(color, delay){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             h1.style.color = color;
-//             resolve("Color changed");
-//         },delay);
-//     });
-// }
-
-// colorChange("red",1000)
-//     .then(()=>{
-//         console.log("red color Completed");
-//         return colorChange("green",1000);
-//     })
-//     .then(()=>{
-//         console.log("green color completed");
-//         return colorChange("Orange",1000);
-//     })
-//     .then(()=>{
-//         console.log("Organge color completed");
-//         return colorChange("blue",1000);
-//     })
-//     .then(()=>{
-//         console.log("Blue color completed")
-//     });
-
-
-// function savetoDb(data){
-//     return new Promise((resolve,reject)=>{
-//         let speed = Math.floor(Math.random()*10)+1;
-//         if(speed>4)
-//             resolve("Success: Data Saved");
-//         else
-//             reject("Failure: Weak Connection, Data not Saved");
-//     });
-// }
-
-// savetoDb("Data1")
-//     .then((result)=>{
-//         console.log("data1Saved");
-//         console.log("Result of Promise: ", result);
-//         return savetoDb("Data2");
-//     })
-//     .then((result)=>{
-//         console.log("data2Saved");
-//         console.log("Result of Promise: ", result);
-//         return savetoDb("Data3");
-//     })
-//     .then((result)=>{
-//         console.log("data3Saved");
-//         console.log("Result of Promise: ", result);
-//     })
-//     .catch((error)=>{
-//         console.log("Promise Rejected!");
-//     });
 
