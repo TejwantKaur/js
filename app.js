662f4377264ceaa2b8761bc1155dbcab1db2a982
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-14 16:53:37 +0530

    colorChangePromise

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-function savetoDb(data){
+h1 = document.querySelector("h1");
+
+function colorChange(color, delay){
     return new Promise((resolve,reject)=>{
-        let speed = Math.floor(Math.random()*10)+1;
-        if(speed>4)
-            resolve("Success: Data Saved");
-        else
-            reject("Failure: Weak Connection, Data not Saved");
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve("Color changed");
+        },delay);
     });
 }
 
-savetoDb("Data1")
-    .then((result)=>{
-        console.log("data1Saved");
-        console.log("Result of Promise: ", result);
-        return savetoDb("Data2");
+colorChange("red",1000)
+    .then(()=>{
+        console.log("red color Completed");
+        return colorChange("green",1000);
     })
-    .then((result)=>{
-        console.log("data2Saved");
-        console.log("Result of Promise: ", result);
-        return savetoDb("Data3");
+    .then(()=>{
+        console.log("green color completed");
+        return colorChange("Orange",1000);
     })
-    .then((result)=>{
-        console.log("data3Saved");
-        console.log("Result of Promise: ", result);
+    .then(()=>{
+        console.log("Organge color completed");
+        return colorChange("blue",1000);
     })
-    .catch((error)=>{
-        console.log("Promise Rejected!");
+    .then(()=>{
+        console.log("Blue color completed")
     });
+
+
+// function savetoDb(data){
+//     return new Promise((resolve,reject)=>{
+//         let speed = Math.floor(Math.random()*10)+1;
+//         if(speed>4)
+//             resolve("Success: Data Saved");
+//         else
+//             reject("Failure: Weak Connection, Data not Saved");
+//     });
+// }
+
+// savetoDb("Data1")
+//     .then((result)=>{
+//         console.log("data1Saved");
+//         console.log("Result of Promise: ", result);
+//         return savetoDb("Data2");
+//     })
+//     .then((result)=>{
+//         console.log("data2Saved");
+//         console.log("Result of Promise: ", result);
+//         return savetoDb("Data3");
+//     })
+//     .then((result)=>{
+//         console.log("data3Saved");
+//         console.log("Result of Promise: ", result);
+//     })
+//     .catch((error)=>{
+//         console.log("Promise Rejected!");
+//     });
 
