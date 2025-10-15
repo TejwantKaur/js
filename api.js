36fc49664c7c557249674e7f0f935b00b0196458
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-15 13:08:05 +0530

    cat api

--- api.js
+++ api.js
@@ -1,27 +1,3 @@
-
-
-// btn.addEventListener("click", async ()=>{
-//     let fact = await getfacts();
-//     console.log(fact);
-    
-//     let p = document.querySelector("#result");
-//     p.innerText = fact;
-// })
-
-// let url = "https://catfact.ninja/fact";
-
-// // using axios
-// async function getfacts(){
-//     try{
-//         let res = await axios.get(url);
-//         return res.data.fact;
-//     } catch(e){
-//         console.log(e);
-//         return "No Fact Found";
-//     }
-    
-// }
-
 // promise + async + await
 // async function getfacts(){
 //     try{
---
+++ cat.js
@@ -0,0 +1,21 @@
+let url = "https://catfact.ninja/fact";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async ()=>{
+    let fact = await getfacts();
+    console.log(fact);
+    
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+});
+
+// using axios
+async function getfacts(){
+    try{
+        let res = await axios.get(url);
+        return res.data.fact;
+    } catch(e){
+        console.log(e);
+        return "No Fact Found";
+    }
+}
