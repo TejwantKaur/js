dc852bd6b5b07dc23a0230139dddc8c82b117125
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-15 12:02:33 +0530

    promise+async+await

--- api.js
+++ api.js
@@ -1,21 +1,42 @@
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
-    .then(res => {
-        // console.log(res);
-        return res.json();
-    })
-    .then((data)=>{
-        console.log("Data1=",data.fact);
-        return fetch(url);
-    })
-    .then((res)=>{
-        return res.json();
-    })
-    .then((data2)=>{
-        console.log("Data2=",data2.fact);
+// promise + async + await
 
-    })
-    .catch((err)=>{
-        console.log(err)
-    });
+async function getfacts(){
+    try{
+    let res = await fetch(url) 
+    // jdo tak fetch ni hunda wait kero, then next kmm
+    let data = await res.json();
+    console.log(data.fact);
+
+    let res2 = await fetch(url)
+    let data2 = await res2.json();
+    console.log(data2.fact);
+
+    }
+    catch(e){
+        console.log(e);
+    }
+    console.log("Bye")
+}
+
+// fetch(url)
+//     .then(res => {
+//         // console.log(res);
+//         return res.json();
+//     })
+//     .then((data)=>{
+//         console.log("Data1=",data.fact);
+//         return fetch(url);
+//     })
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data2)=>{
+//         console.log("Data2=",data2.fact);
+
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     });
+
