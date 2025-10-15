72976f56e90e9b792e8d48ab536e4d36b68ad95f
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-15 11:49:28 +0530

    first api

---
+++ api.js
@@ -0,0 +1,21 @@
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+    .then(res => {
+        // console.log(res);
+        return res.json();
+    })
+    .then((data)=>{
+        console.log("Data1=",data.fact);
+        return fetch(url);
+    })
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data2)=>{
+        console.log("Data2=",data2.fact);
+
+    })
+    .catch((err)=>{
+        console.log(err)
+    });
