e5acf5819169bc4bbe43cee3889e4d75d1be6c2c
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-15 12:27:30 +0530

    first api call, and get random facts on website

--- api.js
+++ api.js
@@ -1,11 +1,25 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+    let fact = await getfacts();
+    console.log(fact);
+    
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+})
+
 let url = "https://catfact.ninja/fact";
 
 // using axios
 async function getfacts(){
-    let res = await axios.get(url);
-    console.log(res.data.fact);
+    try{
+        let res = await axios.get(url);
+        return res.data.fact;
+    } catch(e){
+        console.log(e);
+        return "No Fact Found";
+    }
+    
 }
-
 
 // promise + async + await
 // async function getfacts(){
