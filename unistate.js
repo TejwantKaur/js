6a710f4be42b654a371b4d112b368f841897ef50
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-16 15:15:08 +0530

    new dir

--- unistate.js
+++ unistate.js
@@ -1,2 +1,29 @@
 let btn = document.querySelector("button");
-let url = "http://universities.hipolabs.com/search?name="
+let url = "https://universities.hipolabs.com/search?name=india"
+
+let state = "kerala";
+
+btn.addEventListener("click", async() =>{
+    let colArr = await getColleges();
+    show(colArr);
+});
+
+function show(colArr){
+    for(col of colArr){
+        console.log(col);
+    }
+}
+
+async function getColleges(){
+    try{
+        let res = await axios.get(url);
+        // console.log(res.data);
+        let clgs = res.data.filter(col=>
+            col['state-province'] && col['state-province'].toLowerCase() === state.toLowerCase()
+        );
+        return clgs;
+    }
+    catch(e){
+        console.log(e);
+    }
+}
