0e4eeaeac11b88d5cf9b9c16425be7267dcd0601
TejwantKaur <kaurtejwant998@gmail.com>
2025-10-29 00:52:30 +0530

    full restfull api

--- Backend/rest_api/index.js
+++ Backend/rest_api/index.js
@@ -1,27 +1,40 @@
-const express = require("express");
+// const express = require("express");
+// const path = require("path");
+// const { v4 : uuidv4 } = require("uuid");
+// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+import methodOverride from "method-override";
+
 const app = express();
 const port = 8080;
-const path = require("path");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:"kaur_tejwant",
         content:"I love coding :)"
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username:"watermelon",
         content:"I am Good :)"
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username:"sahib_noor",
         content:"I m Genious :)"
     },
@@ -36,7 +49,8 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let { username, content } = req.body;
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({id, username, content });
     res.redirect("/posts");
 })
 
@@ -45,9 +59,35 @@ app.get("/posts/:id",(req,res)=>{
     // console.log(id);
     let post = posts.find((p)=> id===p.id);
     if (!post) {
-        return res.status(404).send("Post not found");
+        return res.status(404).send("Post not found view content");
     }
     res.render("show.ejs", { post });
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let { id } = req.params;
+    let post = posts.find((p)=> id===p.id);
+    res.render("edit.ejs",{ post });
+})
+
+app.patch("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id===p.id);
+    if (!post) {
+        return res.status(404).send("Post not found in patch");
+    }
+    // console.log(newContent);
+    post.content = newContent;
+    // res.render("show.ejs", { post });
+    res.redirect("/posts");
+});
+
+app.delete("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    // res.send("delete success");
+    posts = posts.filter((p)=> id!=p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port,()=>{
